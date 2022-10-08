c544d1ba5d67a850a07c3ccf1be482aa6c68c85a
Diizzayy <me@diizzayy.com>
2022-10-08 09:01:16 -0400

    2 files modified, 1 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,35 +9,7 @@ export default defineNuxtConfig({
 
   apollo: {
     clients: {
-      default: {
-        // The GraphQL endpoint.
-        httpEndpoint: 'https://api.spacex.land/graphql',
-
-        // Provide a GraphQL endpoint to be used client-side. Overrides `httpEndpoint`.
-        // browserHttpEndpoint: '/graphql',
-
-        // See https://www.apollographql.com/docs/link/links/http.html#options
-        httpLinkOptions: {
-          credentials: 'same-origin',
-        },
-
-        defaultOptions: {
-          watchQuery: {
-            fetchPolicy: 'cache-and-network',
-          },
-        },
-
-        // Specify a websocket endpoint to be used for subscriptions.
-        // The `wss` protocol is recommended in production.
-        // wsEndpoint: 'ws://localhost:4000',
-
-        // LocalStorage token
-        tokenName: 'spacex-token',
-
-        // Specify if the client should solely use WebSocket.
-        // requires `wsEndpoint`.
-        websocketsOnly: false,
-      },
+      default: './apollo/default.ts',
       github: {
         httpEndpoint: 'https://api.github.com/graphql',
         tokenStorage: 'localStorage',
